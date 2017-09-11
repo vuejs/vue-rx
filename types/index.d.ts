cf715ba39d94c9798030054089381c1eee0bdb19
Herrington Darkholme <nonamesheep1@gmail.com>
2017-09-12 00:16:06 +0800

    add initial typing

---
+++ types/index.d.ts
@@ -0,0 +1,28 @@
+import Vue = require('vue')
+import { WatchOptions } from 'vue'
+import { Observable } from 'rxjs/Observable'
+
+export type Observables = Record<string, Observable<any>>
+declare module 'vue/types/options' {
+  interface ComponentOptions<V extends Vue> {
+    subscriptions:  Observables | (() => Observables)
+    domStreams: string[]
+  }
+}
+
+declare module "vue/types/vue" {
+  interface Vue {
+    $observables: Observables;
+    $watchAsObservable(exprOrFn: string | Function, options?: WatchOptions): Observable<any>
+    $eventToObservable(event: string): Observable<any>
+    $subscribeTo<T>(
+      observable: Observable<T>,
+      next: (t: T) => void,
+      error: (e: any) => void,
+      complete: () => void): void
+    $fromDOMEvent(selector: string, event: string): Observable<any>
+    $createObservableMethod(methodName: string): Observable<any>
+  }
+}
+
+export declare function install(V: typeof Vue): void
