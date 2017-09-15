70417c9dcf24544af25629b84f85370b0b5b6af8
HerringtonDarkholme <HerringtonDarkholme@users.noreply.github.com>
2017-09-15 09:51:58 +0800

    feat(typing): use es style module export

---
+++ types/index.d.ts
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import { WatchOptions } from 'vue'
+import { Observable } from 'rxjs/Observable'
+
+export type Observables = Record<string, Observable<any>>
+declare module 'vue/types/options' {
+  interface ComponentOptions<V extends Vue> {
+    subscriptions?: Observables | ((this: V) => Observables)
+    domStreams?: string[]
+    observableMethods?: string[] | Record<string, string>
+  }
+}
+
+export interface WatchObservable<T> {
+  newValue: T
+  oldValue: T
+}
+declare module "vue/types/vue" {
+  interface Vue {
+    $observables: Observables;
+    $watchAsObservable(expr: string, options?: WatchOptions): Observable<WatchObservable<any>>
+    $watchAsObservable<T>(fn: (this: this) => T, options?: WatchOptions): Observable<WatchObservable<T>>
+    $eventToObservable(event: string): Observable<{name: string, msg: any}>
+    $subscribeTo<T>(
+      observable: Observable<T>,
+      next: (t: T) => void,
+      error?: (e: any) => void,
+      complete?: () => void): void
+    $fromDOMEvent(selector: string | null, event: string): Observable<Event>
+    $createObservableMethod(methodName: string): Observable<any>
+  }
+}
+
+export declare function install(V: typeof Vue): void
